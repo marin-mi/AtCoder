843319a1e4d6bf1c1e483efe86c98d2242eb568e
marin-mi <marin0109yuki@gmail.com>
2023-09-02 10:18:32 +0900

    main

---
+++ ABC/284/B.py
@@ -0,0 +1,13 @@
+T = int(input())
+
+for _ in range(T):
+    N = int(input())
+    A = list(map(int, input().split()))
+
+    cnt = 0 
+    
+    for i in range(N):
+        if A[i] % 2 == 1:
+            cnt += 1
+          
+    print(cnt)
