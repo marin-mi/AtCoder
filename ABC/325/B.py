12e12ca26c2fcc85b14b43049211db612af5f2c4
marin-mi <marin0109yuki@gmail.com>
2023-11-01 13:28:49 +0900

    main

--- ABC/325/B.py
+++ ABC/325/B.py
@@ -1,7 +1,18 @@
 N = int(input())
-W, X = [], []
+cnt = [0 for _ in range(24)]
 for _ in range(N):
     w, x = map(int, input().split())
-    W.append(w)
-    X.append(x)
+    cnt[x] += w
 
+ans = 0
+for i in range(24):
+    sum = 0
+    for j in range(9):
+        sum += cnt[(i + j) % 24]
+    ans = max(ans, sum)
+
+print(ans)
+
+# 公式解説
+# cntで24時間の人数を格納する
+# 11行目がよくわからん
