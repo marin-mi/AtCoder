8b54e10b6514402b7b6f960f3d5ee9fa3ee97b36
marin-mi <marin0109yuki@gmail.com>
2023-11-08 20:31:41 +0900

    main

--- ABC/236/A.py
+++ ABC/236/A.py
@@ -1,6 +1,12 @@
-S = input()
+S = list(input())
 a, b = map(int, input().split())
 
-ans = S[:a] + 
+S[a - 1], S[b - 1] = S[b - 1], S[a - 1]
 
-print(ans)
+print(*S, sep='')
+
+# 自分で解いた
+# 要素の入れ替えは4行目の通り
+# 1行目が文字列だと4行目の動作ができない
+# そのためlistにしておく
+# unpackして空白を消す
